597e475234b30f6bfc3b904d799b3f292f54020b
dhruv-dhaduk <dhadukd44@gmail.com>
2024-11-23 19:00:21 +0530

    Added insertion sort

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 extern "C" {
     #include "math/math.h" 
+    #include "001_insertion_sort.h"
 }
 
 TEST(Math, Add) {
@@ -13,6 +14,43 @@ TEST(Math, Substract) {
     EXPECT_EQ(substract(10, 30), -20);
     EXPECT_EQ(substract(40, 30), 10);
     EXPECT_EQ(substract(20, 20), 0);
+}
+
+TEST(T_001_INSERTION_SORT, T1) {
+    int original[] = {5, 2, 4, 6, 1, 3};
+    int sorted[] = {1, 2, 3, 4, 5, 6};
+    int n = 6;
+
+    insertionSort(original, n);
+
+    for (int i = 0; i < n; i++) {
+        EXPECT_EQ(original[i], sorted[i]);
+    }
+}
+
+
+TEST(T_001_INSERTION_SORT, T2_ASCENDING_ORDER) {
+    int original[] = {1, 2, 3, 4, 5, 6};
+    int sorted[] = {1, 2, 3, 4, 5, 6};
+    int n = 6;
+
+    insertionSort(original, n);
+
+    for (int i = 0; i < n; i++) {
+        EXPECT_EQ(original[i], sorted[i]);
+    }
+}
+
+TEST(T_001_INSERTION_SORT, T3_DESCENDING_ORDER) {
+    int original[] = {6, 5, 4, 3, 2, 1};
+    int sorted[] = {1, 2, 3, 4, 5, 6};
+    int n = 6;
+
+    insertionSort(original, n);
+
+    for (int i = 0; i < n; i++) {
+        EXPECT_EQ(original[i], sorted[i]);
+    }
 }
 
 int main(int argc, char **argv) {
---
+++ src/001_insertion_sort.c
@@ -0,0 +1,12 @@
+void insertionSort(int* arr, int n) {
+    for (int i = 1; i < n; i++) {
+        int num = arr[i];
+        int j = i - 1;
+        
+        while (j >= 0 && arr[j] > num) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = num;
+    }
+}
